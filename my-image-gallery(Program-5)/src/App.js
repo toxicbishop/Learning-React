0bb832befe0f5c1876b07eb4aa5ebf40f3d1c539
toxicbishop <92860129+toxicbishop@users.noreply.github.com>
2026-03-09 19:54:24 +0530

    Fixed issues and followed correct steps

--- my-image-gallery(Program-5)/src/App.js
+++ my-image-gallery(Program-5)/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import FigureList from "./components/FigureList";
-import "./App.css";
-function App() {
-  return (
-    <div className="App">
-      <h1>Image Gallery</h1>
-      <FigureList />
-    </div>
-  );
-}
+import React from 'react'; 
+import FigureList from './components/FigureList'; 
+import './App.css'; 
+
+const App = () => { 
+  return ( 
+    <div className="app"> 
+      <h1>Dynamic Image Gallery</h1> 
+      <FigureList /> 
+    </div> 
+  ); 
+}; 
+
 export default App;
